11ff1163c336cd4af56684e49347a641fc08fcd0
tamimishrak <ishraksanjid6@gmail.com>
2024-09-15 19:36:05 +0600

    added a SingleSessionView

--- backend/chatbot/chatbot_app/urls.py
+++ backend/chatbot/chatbot_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ChatSessionsView
+from .views import ChatSessionsView, SingleSessionView
 
 urlpatterns = [
-    path('sessions/', ChatSessionsView.as_view())
+    path('sessions/', ChatSessionsView.as_view()),
+    path('sessions/<str:session_id>/', SingleSessionView.as_view())
 ]
--- backend/chatbot/chatbot_app/views.py
+++ backend/chatbot/chatbot_app/views.py
@@ -5,4 +5,8 @@ from rest_framework.response import Response
 class ChatSessionsView(APIView):
     def get(self, request):
         return Response({"message": "list of all the sessions"}, status=200)
-            
+
+class SingleSessionView(APIView):
+    def get(self, request, session_id) :
+        session_id = 1123123
+        return Response({"message": session_id}, status=200)     
