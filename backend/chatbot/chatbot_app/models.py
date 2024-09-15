b364f0980c27d903b339b510044fe6e91f77faf0
tamimishrak <ishraksanjid6@gmail.com>
2024-09-15 19:04:06 +0600

    created superuser,  models and registered them

--- backend/chatbot/chatbot_app/models.py
+++ backend/chatbot/chatbot_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class ChatSession(models.Model):
+    session_id = models.CharField(max_length=255, unique=True)
+    session_title = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+  
+class Message(models.Model):
+    session = models.ForeignKey(ChatSession, on_delete=models.CASCADE, related_name="messages")
+    text = models.TextField()
+    is_bot = models.BooleanField(default=False)
+    m_created_at = models.DateTimeField(auto_now_add=True)
+    
