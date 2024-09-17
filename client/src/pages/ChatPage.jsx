87bdcd259dc67fd1f42ecaada3ef7167add8ddab
Shahrukh Khan <shahrukh.priv@gmail.com>
2024-09-18 00:40:44 +0600

    added the chatpage

--- client/src/pages/ChatPage.jsx
+++ client/src/pages/ChatPage.jsx
@@ -1,20 +1,47 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 
+import axios from 'axios'
 import Prompt from '../components/Prompt'
 import "./ChatPage.css"
 
 export default function ChatPage(){
+  const [messages, setMessages] = useState([]);
+  const { id } = useParams();
+
+  useEffect(() => {
+      if(id){
+        async function fetchMessages(){
+          try{
+            const response = await axios.get(http://localhost:8000/api/sessions/${id});
+            console.log(response.data);
+            if(response.status === 200){
+              setMessages(response.data);
+            } else {
+              alert('Failed to load messages '+response.status);
+            }
+          } catch(error){
+            console.error(error);
+          }
+        }
+        fetchMessages();
+      }
+  }, [id]);
+
   return (
     <div className="chatPage">
       <div className='wrapper'>
         <div className="chat">
-          <div className='message user'>Hello</div>
-          <div className='message'>Hello welcome to phymate</div>
+          {
+            messages.map((message, index) => (
+              <div key={index} className={`message ${message.is_bot ? '' : 'user'}`}>
+                {message.text}
+              </div>
+            ))
+          }
           <Prompt />
         </div>
       </div>
     </div>
   )
 }
-
-
